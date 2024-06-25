0602e8188fd92d7f0778a2c10959c205436db4fe
S.Ani <115107902+1SZ7@users.noreply.github.com>
2024-06-25 21:20:18 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = 'This text was changed by JavaScript!';
+});
